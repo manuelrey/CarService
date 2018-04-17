468573c12d5a267ed3bd4a2f1c554e02dac15ff8
Manuel Rey <01manuelrey@gmail.com>
2018-04-17 14:47:16 -0400

    Add user properties

--- CarService/Data/Migrations/20180417181655_addUserProperties.Designer.cs
+++ CarService/Data/Migrations/20180417181655_addUserProperties.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CarService.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180417181655_addUserProperties")]
+    partial class addUserProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
